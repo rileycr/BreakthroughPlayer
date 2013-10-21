eb2ee8bebbc98cdedd27fe2848d3c3d0e807a664
Cooper Riley <rileycr@miamioh.edu>
2013-10-21 18:22:37 -0400

    Made the package

---
+++ WhizBang.java
@@ -0,0 +1,18 @@
+package WhizBang;
+
+import game.*;
+
+public class WhizBang extends GamePlayer {
+
+	public WhizBang(String nickname, GameState gs, boolean isDeterministic) {
+		super(nickname, gs, isDeterministic);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public GameMove getMove(GameState state, String lastMv) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
