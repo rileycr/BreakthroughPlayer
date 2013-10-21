896d61752ad5a1a57c16f02c4c71145e0660c1b4
Cooper Riley <rileycr@miamioh.edu>
2013-10-21 18:40:19 -0400

    Added basic methods

--- WhizBang.java
+++ WhizBang.java
@@ -1,18 +1,30 @@
 package WhizBang;
 
+import breakthrough.*;
 import game.*;
 
+/**
+ * Whizbang is an AI breakthrough player for CSE 486.
+ * 
+ * @author Cooper Riley, Josh Ruebusch, Lauren Murray
+ *
+ */
 public class WhizBang extends GamePlayer {
 
-	public WhizBang(String nickname, GameState gs, boolean isDeterministic) {
-		super(nickname, gs, isDeterministic);
-		// TODO Auto-generated constructor stub
+	public WhizBang(String nickname, boolean isDeterministic) {
+		super(nickname, new BreakthroughState(), isDeterministic);
 	}
 
 	@Override
 	public GameMove getMove(GameState state, String lastMv) {
+		BreakthroughState board = (BreakthroughState)state;
+		
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	public static void main(String[] args){
+		GamePlayer player = new WhizBang("The WhizBanger", false);
+		player.compete(args);
+	}
 }
