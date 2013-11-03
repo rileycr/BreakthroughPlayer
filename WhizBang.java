6eb5140c3a567b118ddbc5f5514505aa51a6c26e
Cooper Riley <rileycr@miamioh.edu>
2013-11-03 14:08:14 -0500

    Implementation of alpha-beta search. Still needs testing

--- WhizBang.java
+++ WhizBang.java
@@ -11,17 +11,28 @@ import game.*;
  */
 public class WhizBang extends GamePlayer {
 
+	protected ScoredBreakthroughMove[] moveStack;
+	public static final double MAX_SCORE = Double.MAX_VALUE;
+	public static final int MAX_DEPTH = 5; // TODO
+
 	public WhizBang(String nickname, boolean isDeterministic) {
 		super(nickname, new BreakthroughState(), isDeterministic);
 	}
 
 	@Override
 	public GameMove getMove(GameState state, String lastMv) {
-		BreakthroughState board = (BreakthroughState) state;
+		initalizeStack();
+		alphaBeta((BreakthroughState) state, 0, Double.NEGATIVE_INFINITY,
+				Double.POSITIVE_INFINITY);
+		return moveStack[0];
 
-		// TODO Scoring and move decision
+	}
 
-		return null;
+	public void initalizeStack() {
+		moveStack = new ScoredBreakthroughMove[MAX_DEPTH];
+		for (int i = 0; i < MAX_DEPTH; i++) {
+			moveStack[i] = new ScoredBreakthroughMove();
+		}
 	}
 
 	/**
@@ -38,7 +49,103 @@ public class WhizBang extends GamePlayer {
 	 */
 	public void alphaBeta(BreakthroughState board, int currentDepth,
 			double alpha, double beta) {
-		// TODO the method for doing the a-b search
+
+		boolean toMaximize = (board.getWho() == GameState.Who.HOME);
+		boolean isTerminal = terminalValue(board, moveStack[currentDepth]);
+
+		if (isTerminal) {
+			;
+		} else if (currentDepth == MAX_DEPTH - 1) {
+			moveStack[currentDepth].score = evaluate(board);
+		} else {
+			char whoseTurn;
+			int dRow; // The change in row for a turn
+			if (toMaximize) {
+				whoseTurn = BreakthroughState.homeSym;
+				dRow = 1;
+			} else {
+				whoseTurn = BreakthroughState.awaySym;
+				dRow = -1;
+			}
+
+			double bestScore = (toMaximize ? Double.NEGATIVE_INFINITY
+					: Double.POSITIVE_INFINITY);
+			ScoredBreakthroughMove tempMove = new ScoredBreakthroughMove();
+			ScoredBreakthroughMove bestMove = moveStack[currentDepth];
+			ScoredBreakthroughMove nextMove = moveStack[currentDepth + 1];
+			bestMove.score = bestScore;
+			GameState.Who currentTurn = board.getWho();
+
+			for (int i = 0; i < BreakthroughState.N; i++) {
+				for (int j = 0; j < BreakthroughState.N; j++) {
+					if (board.board[i][j] == whoseTurn) {
+						tempMove.startRow = i;
+						tempMove.startCol = j;
+						tempMove.endingRow = i + dRow;
+
+						for (int k = -1; k < 2; k++) {
+							tempMove.endingCol = j + k;
+							if (ScoredBreakthroughMove
+									.indexOK(tempMove.endingCol)
+									&& board.board[tempMove.endingRow][tempMove.endingCol] != whoseTurn) {
+
+								BreakthroughState tempBoard = (BreakthroughState) board
+										.clone();
+								tempBoard.makeMove(tempMove);
+								alphaBeta(tempBoard, currentDepth + 1, alpha,
+										beta);
+
+								/*
+								 * char currentlyThere =
+								 * board.board[tempMove.endingCol
+								 * ][tempMove.endingRow];
+								 * board.makeMove(tempMove);
+								 * 
+								 * alphaBeta(board, currentDepth + 1, alpha,
+								 * beta);
+								 * 
+								 * board.board[tempMove.endingCol][tempMove.
+								 * endingRow] = currentlyThere;
+								 * board.board[tempMove
+								 * .startCol][tempMove.startRow] = whoseTurn;
+								 */
+
+								board.status = GameState.Status.GAME_ON;
+								board.who = currentTurn;
+
+								// Updates the best score
+								if (toMaximize
+										&& nextMove.score > bestMove.score) {
+									bestMove = (ScoredBreakthroughMove) tempMove
+											.clone();
+									bestMove.score = nextMove.score;
+								} else if (!toMaximize
+										&& nextMove.score < bestMove.score) {
+									bestMove = (ScoredBreakthroughMove) tempMove
+											.clone();
+									bestMove.score = nextMove.score;
+								}
+
+								// Performing pruning
+								if (toMaximize) {
+									alpha = Math.max(bestMove.score, alpha);
+									if (bestMove.score >= beta
+											|| bestMove.score == MAX_SCORE) {
+										return;
+									}
+								} else {
+									beta = Math.min(bestMove.score, beta);
+									if (bestMove.score <= alpha
+											|| bestMove.score == -MAX_SCORE) {
+										return;
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
 	}
 
 	/**
@@ -49,28 +156,26 @@ public class WhizBang extends GamePlayer {
 	 *            the board to evaluate
 	 * @return the score of the board (+ home winning, - away winning)
 	 */
-	public int evaluate(BreakthroughState board) {
-		// TODO We can figure out this once we write all our scoring methods.
-		return 0;
+	public double evaluate(BreakthroughState board) {
+		// TODO a preliminary implementation just to do some testing.
+		return forwardTriangleEval(board) + detectBlock(board);
 	}
 
 	/**
 	 * Determines a score based on the number of opposing pieces in front and
-	 * diagonal to the player's pieces. Will be run once for each player.
+	 * diagonal to the player's pieces. Will be run once.
+	 * 
+	 * Calculates the difference of home and away's scores internally.
 	 * 
 	 * @param board
-	 *            the board which is being examined
-	 * @param player
-	 *            which player are we evaluating for
-	 * @return the score for player
+	 *            the board to be evaluated
+	 * @return the combined score of home and away
 	 */
-	public int forwardTriangleEval(BreakthroughState board, char player) {
-		int score = 0;
-
-		// TODO Adjust score to allow for weighted scoring
+	public double forwardTriangleEval(BreakthroughState board) {
+		double score = 0;
 		for (int row = 0; row < BreakthroughState.N; row++) {
 			for (int col = 0; col < BreakthroughState.N; col++) {
-				if (board.board[row][col] == player) {
+				if (board.board[row][col] != BreakthroughState.emptySym) {
 					score += forwardTriAtPiece(board, row, col);
 				}
 			}
@@ -80,7 +185,7 @@ public class WhizBang extends GamePlayer {
 
 	/**
 	 * Used by forwardTriangleEval function, counts the opposing pieces in front
-	 * of a specific player piece.
+	 * of the piece.
 	 * 
 	 * @param board
 	 *            to be examined
@@ -90,25 +195,25 @@ public class WhizBang extends GamePlayer {
 	 *            of piece we are looking at
 	 * @return number of opposing pieces in front of that piece
 	 */
-	public int forwardTriAtPiece(BreakthroughState board, int row, int col) {
-		int count = 0;
+	public double forwardTriAtPiece(BreakthroughState board, int row, int col) {
+		double count = 0;
 
-		if (board.who == GameState.Who.HOME) {
+		if (board.board[row][col] == BreakthroughState.homeSym) {
 			for (int i = row + 1; i < BreakthroughState.N; i++) {
-				for (int j = row - i; j <= i - row; j++) {
+				for (int j = row - i; j < i - row; j++) {
 					if (col + j >= 0
 							&& col + j < BreakthroughState.N
 							&& board.board[i][col + j] == BreakthroughState.awaySym) {
-						count++;
+						count--;
 					}
 				}
 			}
 		} else {
 			for (int i = row - 1; i >= 0; i--) {
-				for (int j = i - row; j <= row - i; j++) {
+				for (int j = i - row; j < row - i; j++) {
 					if (col + j >= 0
 							&& col + j < BreakthroughState.N
-							&& board.board[i][col + j] == BreakthroughState.awaySym) {
+							&& board.board[i][col + j] == BreakthroughState.homeSym) {
 						count++;
 					}
 				}
@@ -125,8 +230,8 @@ public class WhizBang extends GamePlayer {
 	 *            state is examined for scoring
 	 * @return score positive if in home favor and negative if away favor
 	 */
-	public int detectBlock(BreakthroughState board) {
-		int score = 0;
+	public double detectBlock(BreakthroughState board) {
+		double score = 0;
 		for (int i = 0; i < BreakthroughState.N; i++) {
 			for (int j = 0; j < BreakthroughState.N; j++) {
 				if (board.board[i][j] == BreakthroughState.homeSym
@@ -147,8 +252,53 @@ public class WhizBang extends GamePlayer {
 		return score;
 	}
 
+	public boolean terminalValue(BreakthroughState board,
+			ScoredBreakthroughMove move) {
+		GameState.Status status = board.getStatus();
+		boolean isTerminal = true;
+
+		if (status == GameState.Status.HOME_WIN) {
+			move.score = MAX_SCORE;
+		} else if (status == GameState.Status.AWAY_WIN) {
+			move.score = -MAX_SCORE;
+		} else if (status == GameState.Status.DRAW) {
+			move.score = 0;
+		} else {
+			isTerminal = false;
+		}
+
+		return isTerminal;
+	}
+
 	public static void main(String[] args) {
 		GamePlayer player = new WhizBang("The WhizBanger", false);
 		player.compete(args);
+	}
+
+	/**
+	 * Extends BreakthroughMove to allow for scoring of moves
+	 * 
+	 * @author Cooper
+	 * 
+	 */
+	class ScoredBreakthroughMove extends BreakthroughMove {
+		public double score;
+
+		public ScoredBreakthroughMove(int r1, int c1, int r2, int c2,
+				double score) {
+			super(r1, c1, r2, c2);
+			this.score = score;
+		}
+
+		public ScoredBreakthroughMove() {
+			super();
+			this.score = 0;
+		}
+
+		public Object clone() {
+			return new ScoredBreakthroughMove(startRow, startCol, endingRow,
+					endingCol, score);
+
+		}
 	}
 }
