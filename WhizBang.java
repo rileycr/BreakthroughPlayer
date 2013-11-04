f5ee8b3db478b47a9a286ba99c2772514f93d5d7
Cooper Riley <rileycr@miamioh.edu>
2013-11-04 18:53:56 -0500

    Shuffling possible moves implementedgit add WhizBang.java

--- WhizBang.java
+++ WhizBang.java
@@ -1,5 +1,7 @@
 package WhizBang;
 
+import java.util.*;
+
 import breakthrough.*;
 import game.*;
 
@@ -10,14 +12,21 @@ import game.*;
  * 
  */
 public class WhizBang extends GamePlayer {
-	
+
 	protected ScoredBreakthroughMove[] moveStack;
 	public static final double MAX_SCORE = Double.MAX_VALUE;
-	public static final int MAX_DEPTH = 7; // TODO
-       // Board values for the home team
-       private static final int[][] homeValues = {{5,15,15,5,15,15,5},{2,3,3,3,3,3,2},{4,5,5,5,5,5,4},{8,10,10,10,10,10,8},{12,15,15,15,15,15,12},{20,25,25,25,25,25,20},{45,45,45,45,45,45,45}};
-       // Board values for the away team
-       private static final int[][] awayValues = {{45,45,45,45,45,45,45},{20,25,25,25,25,25,20},{12,15,15,15,15,15,12},{8,10,10,10,10,10,8},{4,5,5,5,5,5,4},{2,3,3,3,3,3,2},{5,15,15,5,15,15,5}};
+	public static final int MAX_DEPTH = 6; // TODO
+	// Board values for the home team
+	private static final int[][] homeValues = { { 5, 15, 15, 5, 15, 15, 5 },
+			{ 2, 3, 3, 3, 3, 3, 2 }, { 4, 5, 5, 5, 5, 5, 4 },
+			{ 8, 10, 10, 10, 10, 10, 8 }, { 12, 15, 15, 15, 15, 15, 12 },
+			{ 20, 25, 25, 25, 25, 25, 20 }, { 45, 45, 45, 45, 45, 45, 45 } };
+	// Board values for the away team
+	private static final int[][] awayValues = { { 45, 45, 45, 45, 45, 45, 45 },
+			{ 20, 25, 25, 25, 25, 25, 20 }, { 12, 15, 15, 15, 15, 15, 12 },
+			{ 8, 10, 10, 10, 10, 10, 8 }, { 4, 5, 5, 5, 5, 5, 4 },
+			{ 2, 3, 3, 3, 3, 3, 2 }, { 5, 15, 15, 5, 15, 15, 5 } };
+
 	public WhizBang(String nickname, boolean isDeterministic) {
 		super(nickname, new BreakthroughState(), isDeterministic);
 	}
@@ -64,7 +73,8 @@ public class WhizBang extends GamePlayer {
 		} else if (currentDepth == MAX_DEPTH - 1) {
 			moveStack[currentDepth].score = evaluate(board);
 		} else {
-			double bestScore = (toMaximize ? Double.NEGATIVE_INFINITY : Double.POSITIVE_INFINITY);
+			double bestScore = (toMaximize ? Double.NEGATIVE_INFINITY
+					: Double.POSITIVE_INFINITY);
 
 			char whoseTurn;
 			int dRow; // The change in row for a turn
@@ -81,6 +91,8 @@ public class WhizBang extends GamePlayer {
 			ScoredBreakthroughMove nextMove = moveStack[currentDepth + 1];
 			bestMove.score = bestScore;
 
+			ArrayList<ScoredBreakthroughMove> possibleMoves = new ArrayList<ScoredBreakthroughMove>();
+
 			for (int i = 0; i < BreakthroughState.N; i++) {
 				for (int j = 0; j < BreakthroughState.N; j++) {
 					if (board.board[i][j] == whoseTurn) {
@@ -92,39 +104,41 @@ public class WhizBang extends GamePlayer {
 							tempMove.endingCol = j + k;
 
 							if (board.moveOK(tempMove)) {
-								BreakthroughState tempBoard = (BreakthroughState) board
-										.clone();
-								tempBoard.makeMove(tempMove);
-								alphaBeta(tempBoard, currentDepth + 1, alpha,
-										beta);
-								
-								// Updates the best score
-								if (toMaximize
-										&& nextMove.score > bestMove.score) {
-									bestMove.set(tempMove);
-									bestMove.score = nextMove.score;
-								} else if (!toMaximize
-										&& nextMove.score < bestMove.score) {
-									bestMove.set(tempMove);
-									bestMove.score = nextMove.score;
-								}
+								possibleMoves.add((ScoredBreakthroughMove) tempMove.clone());
 
-								// Performing pruning
-								if (toMaximize) {
-									alpha = Math.max(bestMove.score, alpha);
-									if (bestMove.score >= beta
-											|| bestMove.score == MAX_SCORE) {
-										return;
-									}
-								} else {
-									beta = Math.min(bestMove.score, beta);
-									if (bestMove.score <= alpha
-											|| bestMove.score == -MAX_SCORE) {
-										return;
-									}
-								}
 							}
 						}
+					}
+				}
+			}
+
+			Collections.shuffle(possibleMoves);
+
+			for (ScoredBreakthroughMove move : possibleMoves) {
+
+				BreakthroughState tempBoard = (BreakthroughState) board.clone();
+				tempBoard.makeMove(move);
+				alphaBeta(tempBoard, currentDepth + 1, alpha, beta);
+
+				// Updates the best score
+				if (toMaximize && nextMove.score > bestMove.score) {
+					bestMove.set(move);
+					bestMove.score = nextMove.score;
+				} else if (!toMaximize && nextMove.score < bestMove.score) {
+					bestMove.set(move);
+					bestMove.score = nextMove.score;
+				}
+
+				// Performing pruning
+				if (toMaximize) {
+					alpha = Math.max(bestMove.score, alpha);
+					if (bestMove.score >= beta || bestMove.score == MAX_SCORE) {
+						return;
+					}
+				} else {
+					beta = Math.min(bestMove.score, beta);
+					if (bestMove.score <= alpha || bestMove.score == -MAX_SCORE) {
+						return;
 					}
 				}
 			}
@@ -141,7 +155,8 @@ public class WhizBang extends GamePlayer {
 	 */
 	public double evaluate(BreakthroughState board) {
 		// TODO a preliminary implementation just to do some testing.
-		return positionalEval(board) + forwardTriangleEval(board) + detectBlock(board);
+		return positionalEval(board) + forwardTriangleEval(board)
+				+ detectBlock(board);
 	}
 
 	/**
@@ -204,33 +219,34 @@ public class WhizBang extends GamePlayer {
 		}
 		return count;
 	}
-	 /**
-         * Part of the evaluation function used by the alpha-beta search method.
-         * It is called by evaluate(), and it returns the value of the board based on 
-         * the position of the pieces. 
-         * @param board
-         * 			the board to evaluate
-         * @return - value of the board
-         */
-        public double positionalEval(BreakthroughState board){
-        	double boardScore = 0;
-        	int pieceCount = 0;
-        	for(int row = 0; row < BreakthroughState.N; row++){
-        		for(int col = 0; col < BreakthroughState.N; col++){
-        			if(board.board[row][col] == BreakthroughState.homeSym){
-        				pieceCount += 1;
-        				boardScore += homeValues[row][col];
-        			}
-        			else if(board.board[row][col] == BreakthroughState.awaySim){
-        				pieceCount -= 1;
-        				boardScore -= awayValues[row][col];
-        			}
-        		}
-        		// If we have an uneven number of pieces on our board, update score
-        		boardScore += pieceCount * 1.5;
-        	}
-        	return boardScore;
-        }
+
+	/**
+	 * Part of the evaluation function used by the alpha-beta search method. It
+	 * is called by evaluate(), and it returns the value of the board based on
+	 * the position of the pieces.
+	 * 
+	 * @param board
+	 *            the board to evaluate
+	 * @return - value of the board
+	 */
+	public double positionalEval(BreakthroughState board) {
+		double boardScore = 0;
+		int pieceCount = 0;
+		for (int row = 0; row < BreakthroughState.N; row++) {
+			for (int col = 0; col < BreakthroughState.N; col++) {
+				if (board.board[row][col] == BreakthroughState.homeSym) {
+					pieceCount += 1;
+					boardScore += homeValues[row][col];
+				} else if (board.board[row][col] == BreakthroughState.awaySym) {
+					pieceCount -= 1;
+					boardScore -= awayValues[row][col];
+				}
+			}
+			// If we have an uneven number of pieces on our board, update score
+			boardScore += pieceCount * 1.5;
+		}
+		return boardScore;
+	}
 
 	/**
 	 * Determines the score based on pieces that are blocked. Runs only once for
@@ -293,13 +309,13 @@ public class WhizBang extends GamePlayer {
 	public static void main(String[] args) {
 		GamePlayer player = new WhizBang("The WhizBanger", false);
 
-		player.compete(args);
+		 player.compete(args);
 
 		// Used for testing
-	//	BreakthroughState st = new BreakthroughState();
-	//	player.init();
-	//	GameMove mv = player.getMove(st, "");
-	//	System.out.println(mv);
+		//BreakthroughState st = new BreakthroughState();
+		//player.init();
+		//GameMove mv = player.getMove(st, "");
+		//System.out.println(mv);
 
 	}
 
@@ -321,6 +337,12 @@ public class WhizBang extends GamePlayer {
 		public ScoredBreakthroughMove() {
 			super();
 			this.score = 0;
+		}
+
+		public Object clone() {
+			return new ScoredBreakthroughMove(startRow, startCol, endingRow,
+					endingCol, score);
+
 		}
 
 		public void set(ScoredBreakthroughMove mv) {
